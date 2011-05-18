d8618d527fb7d50f678039303a0f2561945340c6
Fernando Ultremare <fernando@6spot.com.br>
2011-05-18 08:37:58 -0300

    v0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ozzob::Application.config.secret_token = 'c5a46b1f6eda61c190cf97cd2b18254f65cd1fd7b0d606313b27b31626d7d9bae5252d27947b97eef70efed872d16f13cd1139659f566fd2d3a0b407f81ea388'
